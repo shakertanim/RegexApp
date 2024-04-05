c8123fbcb1147b1bd73ff36ddcd41f1bf6be412d
shaker <tanimshaker@gmail.com>
2024-04-04 23:19:23 -0500

    Regex App Skeleton Done....

--- RegexApp/Program.cs
+++ RegexApp/Program.cs
@@ -4,6 +4,24 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        
+        bool exitOrContinue = true;
+        while (exitOrContinue)
+        {
+            Console.Write("Enter a regular expression (or press ENTER to use the default): ");
+            string expression = Console.ReadLine();
+            Console.Write("Enter some input: ");
+            string text = Console.ReadLine();
+            RegexApp r = new RegexApp(text, expression);
+
+
+
+            r.regexProgram();
+
+            ConsoleKeyInfo key = Console.ReadKey();
+            if (key.Key == ConsoleKey.Escape)
+            {
+                exitOrContinue = false;
+            }
+        }
     }
 }
--- RegexApp/RegexApp.cs
+++ RegexApp/RegexApp.cs
@@ -1,6 +1,29 @@
 ﻿namespace Regex;
 
+using System.Text.RegularExpressions;
 class RegexApp
 {
-    
+    string inputExpression;
+    string regularExpression;
+
+    public RegexApp(string inputExpression, string regularExpression)
+    {
+        this.inputExpression = inputExpression;
+        this.regularExpression = regularExpression;
+    }
+
+    public void regexProgram()
+    {
+        Regex obj = new Regex(this.regularExpression);
+        bool result;
+        result = obj.IsMatch(this.inputExpression);
+        if (result)
+        {
+            Console.WriteLine($"{this.inputExpression} matches {this.regularExpression}? {result}");
+        }
+        else
+        {
+            Console.WriteLine($"{this.inputExpression} matches {this.regularExpression}? {result}");
+        }
+    }
 }
