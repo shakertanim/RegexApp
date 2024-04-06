efe20f564e9321d0f266d445c9d4bcdfac6fb00e
shaker <tanimshaker@gmail.com>
2024-04-06 02:05:35 -0500

    Completed App....

--- RegexApp/Program.cs
+++ RegexApp/Program.cs
@@ -7,18 +7,20 @@ public class Program
         bool exitOrContinue = true;
         while (exitOrContinue)
         {
-            Console.Write("Enter a regular expression (or press ENTER to use the default): ");
+            Console.Write("Enter a regular expression (or press ENTER to use the default ^[a-z]{1}$): ");
             string expression = Console.ReadLine();
+            
+            if (string.IsNullOrEmpty(expression))
+            {
+                expression = "^[a-z]{1}$";
+            }
+            
             Console.Write("Enter some input: ");
             string text = Console.ReadLine();
-            RegexApp r = new RegexApp(text, expression);
-
-
-
-            r.regexProgram();
-
-            ConsoleKeyInfo key = Console.ReadKey();
-            if (key.Key == ConsoleKey.Escape)
+            RegexApp regexValidate = new RegexApp(text, expression);
+            regexValidate.regexProgram();
+            Console.WriteLine("Press ESC to end or any key to try again.");
+            if (Console.ReadKey().Key == ConsoleKey.Escape)
             {
                 exitOrContinue = false;
             }
